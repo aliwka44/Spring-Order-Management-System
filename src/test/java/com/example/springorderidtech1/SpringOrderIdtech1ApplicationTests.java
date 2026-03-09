74bedfbf0d099df5a3dd19949988ae9ab782c115
alishukurovdev <snin_GitHub_email_adresin>
2026-03-09 13:09:05 +0400

    order management

---
+++ src/test/java/com/example/springorderidtech1/SpringOrderIdtech1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springorderidtech1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOrderIdtech1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
